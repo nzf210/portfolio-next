a1527816e3e0b2100f2a47917f6bb4cf7c534d52
Syahril S <syahrilsymd@gmail.com>
2023-09-21 11:59:09 +0800

    change tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,21 +3,26 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import { Provider } from "./components/Provider";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Syahril Syamsuddin",
   description: "my portfolio app",
+  icons: {
+    icon: "/icon.png"
+  }
 };
 
-export default function RootLayout({
+const RootLayout: React.FC<{ children: React.ReactNode }> = ({
   children,
 }: {
   children: React.ReactNode;
-}) {
+}) => {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Header />
       <body
         className={`${inter.className} bg-white text-black dark:bg-[#090908] dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}
       >
@@ -30,4 +35,17 @@ export default function RootLayout({
       </body>
     </html>
   );
+}
+
+export default RootLayout;
+
+const Header: React.FC = () => {
+  return (
+    <Head>
+      {typeof metadata.title === 'string' && metadata.title && <title>{metadata.title.toString()}</title>}
+      {typeof metadata.title === 'string' && metadata.description && metadata.title && <meta name={metadata.title.toString()} content={metadata.description.toString()} />}
+      <link rel="icon" href="/icon.png" />
+    </Head>
+  );
+
 }
