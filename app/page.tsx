cf83b29db55c7459e10f441d75753ee8aebd1e6c
Syahril S <syahrilsymd@gmail.com>
2023-09-24 22:46:54 +0800

    add tech stack

--- app/page.tsx
+++ app/page.tsx
@@ -107,7 +107,7 @@ export default async function Home() {
           }
         </div>
       </div>
-      <div>
+      <div className="pt-8">
         <TechSTack data={dataLogo} />
       </div>
     </div>
