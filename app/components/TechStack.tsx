41159adf264e2a36ba424db1be405a468d06c321
Syahril S <syahrilsymd@gmail.com>
2023-09-25 00:18:20 +0800

    refactor

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -7,11 +7,13 @@ async function TechStack({ data }: any) {
             <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
                 <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">Tech stack</h2>
                 <div className="grid grid-cols-4 gap-8 text-gray-500 sm:gap-6 md:grid-cols-6 lg:grid-cols-12 dark:text-gray-400">
-                    <Suspense fallback={<h1 className='mx-auto text-center items-center self-center'>Loading...</h1>}>
+                    <Suspense fallback={<h1 className='mx-auto text-center items-center self-center flex'>Loading...</h1>}>
                         {
                             data.map((img: any, index: number) => (
                                 <React.Fragment key={`${index.toString()}`}>
-                                    <Image src={img} alt='img' width={50} height={50} className='rounded-lg' />
+                                    <div className='flex'>
+                                        <Image src={img} alt='img' width={50} height={50} className='rounded-lg mx-auto items-center' />
+                                    </div>
                                 </React.Fragment>
                             ))
                         }
